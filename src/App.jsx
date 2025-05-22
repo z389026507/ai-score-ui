38a27ddf79e39eea6ed109769fdbf838115650fe
z389026507 <zhuangjiangshan@gmail.com>
2025-05-22 18:05:03 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>视觉评分系统已部署成功</h1>
+      <p>你可以接入 GPT 接口或自定义打分逻辑。</p>
+    </div>
+  );
+}
