a46e4442fc5e8c120d9438f042dc9160a0db32d4
z389026507 <zhuangjiangshan@gmail.com>
2025-05-23 10:42:45 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,1 @@
-
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: {} }, plugins: [] };
