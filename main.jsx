f588f7da4876b21890964553cdecc85151bca3b5
z389026507 <zhuangjiangshan@gmail.com>
2025-05-23 11:46:27 +0800

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,1 +1,5 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './src/App'; import './index.css'; ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App';
+import './index.css';
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
